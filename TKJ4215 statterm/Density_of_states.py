1c3d02a4d49e8a540f76f642361a6ab6e4a912cf
Viljar Femoen <viljar.femoen@hotmail.no>
2021-03-03 21:27:41 +0100

    I am speed

--- TKJ4215 statterm/Density_of_states.py
+++ TKJ4215 statterm/Density_of_states.py
@@ -2,7 +2,9 @@ from ast import Num
 import numpy as np
 import itertools as it
 import matplotlib.pyplot as plt
-import pickle
+import pickle, time
+from numba import jit
+from sympy.utilities.iterables import multiset_permutations
 
 def Lattice_shape(N): #The function determines the lattice shapes that you will be using in this exercise, given N particles.
     if N == 4:
@@ -19,33 +21,48 @@ def Lattice_shape(N): #The function determines the lattice shapes that you will 
         return (5,4)
     elif N == 24:
         return (6,4)
+    elif N == 30:
+        return (6,5)
 
-
+# jit oversetter til maskinkode, som gjør ting superraskt
+# kan brukes på det meste av numpy. Måtte lage en shift-funksjonalitet selv tho
+# Selve funksjonen shifter hele arrayen ett hakk opp i y_shift og ett hakk til venstre i x_shift,
+# og xor-er de med Lattice. Det gir samme effekt som å xor-e hvert element med naboene og summe 
+@jit(nopython = True) 
 def Count_AB(Lattice):
-    #Should take an array, representing the lattice, and find the number of AB-interactions. Use PERIODIC BOUNDARY conditions!
-    #Tip: Make sure to avoid double counting. Concider only particles of a given type and count the oposite neighbours.
-    m_AB = 0
-    for y in range(Lattice.shape[0]):
-        for x in range(Lattice.shape[1]):
-                m_AB += 1 if Lattice[y-1, x] != Lattice[y, x] else 0
-                m_AB += 1 if Lattice[y, x-1] != Lattice[y, x] else 0     
-    return m_AB
+    y_shift = np.concatenate((Lattice[-1:, :], Lattice[:-1, :]), axis = 0) ^ Lattice
+    x_shift = np.concatenate((Lattice[:, -1:], Lattice[:, :-1]), axis = 1) ^ Lattice
+    return np.count_nonzero(y_shift) + np.count_nonzero(x_shift) 
 
+
+# denne er så syk nå at jeg nesten ikke skjønner den selv lengre
+def generate(N):
+    # ints: en liste av 2^(N/2-1) heltall, tuklet med så hvert tall er delt opp i to bytes
+    # vi halverer antallet her ved å legge til den ferdige tellingen to ganger, siden resultatet er likt for logcal not
+    ints = np.flip(np.arange(0, 2**(N//2-1), dtype = np.uint16).reshape(2**(N//2-1), 1).view(np.uint8), axis = 1)
+    # perms: liste med alle mulige permutasjoner av lengde N/2, der element i har i antall "False"
+    perms = [list(multiset_permutations(l)) for l in [[0]*i + [1] * (N//2-i) for i in range(N//2)]]
+    # ints, og tolk tallene som en liste av bools heller enn et binært tall
+    for v in np.unpackbits(ints, axis = 1)[:, -N//2:]:
+        # kombiner hver mulige første halvdel av den fullstendige listen med alle mulige andre halvdeler 
+        # slik at total mengde "True" er N/2
+        for v_ in perms[np.count_nonzero(v)]:
+            yield np.concatenate((v, v_))
+
+    
 
 def Create_arrays_and_count(Number_of_particles):
     shape = Lattice_shape(Number_of_particles)
-    m_AB = []#A list that keeps track of the number of AB-interactions for each configuration.
-    for config in it.product(it.product([0, 1], repeat = shape[1]), repeat = shape[0]): #Use itertools.product() to generate all possible unique lattice configurations.
-            #itertools returns all unique combinations, including thos with N_A not = N_B. Select those lists where N_A = N_B
-            #itertools returns a list so you will have to convert the list to an array below.
-            if np.sum(np.array(config)) == Number_of_particles /2:
-                config = np.array(config).reshape(shape)
-                m_AB.append(Count_AB(config))#should append the result of calling Count_AB(Lattice) on a given configuration config
+    m_AB = []
+    for config in generate(Number_of_particles): 
+            e = Count_AB(config.reshape(shape))
+            m_AB.append(e)
+            m_AB.append(e) # skip checking !config (the logical not of config)
     return m_AB
 
 
-
-for i in [4, 6, 8, 12, 16, 20, 24]: #You will be calculating the density of states for the given system sizes
+s = time.time()
+for i in [30]: #You will be calculating the density of states for the given system size
     m_AB = Create_arrays_and_count(i) #Call the correct function to create a list of all possible configuration AB-interactions
     Available_microstates = list(set(m_AB)) #Finds all uniqe macrostates
     Available_microstates.sort() #Sorts this list in ascending order, for plotting purposes.
@@ -63,7 +80,7 @@ for i in [4, 6, 8, 12, 16, 20, 24]: #You will be calculating the density of stat
     Normalization_factor = max(Available_microstates)#The normalization factor should be the highest possible number of AB-interactions for a given system size
     Normalized_mAB = [i/Normalization_factor for i in Available_microstates]#Create a normalized version of m_AB by deviding each instance by the Normalization_factor
     print(np.var(Normalized_mAB)) #Calculate and print the variance of Normalized_mAB. Use np.var().
-
+print(time.time() - s)
 
 #Saves m_AB and i for future use for system size = i_max (in this case 24)
 with open('m_AB.pkl', 'wb') as f:
