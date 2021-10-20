21e690d23f8684150f1a5c6476cda404ab72f083
Viljar <viljar.femoen@hotmail.no>
2021-10-20 11:30:23 +0200

    Did the easy tasks

--- TFY4335_bionano/lab/textprocess.py
+++ TFY4335_bionano/lab/textprocess.py
@@ -48,38 +48,56 @@ def readfile(filename: str) -> list[Point, float]:
     return res[1:]
 
 
-def get_ms(data: list[tuple]) -> float:
-    """For a given list of points, find the mean of squared distances between concecutive points"""
+def get_D(data: list[tuple]) -> tuple[float, float]:
+    """For a given list of points, find the D. Return mu and sigma"""
     p0 = data.pop(0)
-    res = 0
+    Ds = []
     for p in data:
         dx = (p[0] - p0[0])*0.25*10**-6
         dy = (p[1] - p0[1])*0.25*10**-6
-        res += dx**2 + dy**2
+        D = (dx**2 + dy**2)/2*7.5
+        if D < 10**-11:
+            Ds.append(D)
         p0 = p
-    return res / len(data)
+    log_D = np.log(Ds)
+    mu = np.mean(log_D)
+    sigma = np.std(log_D)
+
+    return np.exp(mu + 0.5*sigma**2), (np.exp(sigma**2)-1)*np.exp(2*mu + sigma**2)**0.5
 
 
 def main():
     for fileext in ["1A", "2A", "3A", "1B", "2B", "3B"]:
         curdir = os.path.dirname(__file__)
         data = readfile(os.path.join(curdir, f"Traj_DF_40x_7.5fps_{fileext}.txt"))
-        ms = []
         Ds = []
         for path, size in data:
             if size > 6:
-                ms.append(get_ms(path))
-                Ds.append(ms[-1] / 2 * 7.5/size)
-        ms = np.array(ms)
+                m, s = get_D(path)
+                if m < 10**-11:
+                    Ds.append((m, s))
+
         Ds = np.array(Ds)
+        """
+        l = 1/np.mean(np.log(Ds[:, 0]))
+        D = sum([i*l*np.exp(-l) for i in Ds[:, 0]])
+        # f(y) = l*e^(-l*ln(y))/y
+        # l er fra 1/forventning
+        
         log_Ds = np.log(Ds)
         mu = np.mean(log_Ds)
         sigma = np.std(log_Ds)
         D = np.exp(mu + 0.5*sigma**2)
         D_var = (np.exp(sigma**2)-1)*np.exp(2*mu + sigma**2)**0.5
         r = 2.08 * 10**-19 / D
-        r_var = 2.08 * 10**-19 / (np.exp(sigma**2)-1)*np.exp(-2*mu + sigma**2)**0.5
-        print(f"{fileext} & {len(ms)} & {np.mean(ms)*10**12 :.2g} $\pm$ {np.std(ms)*10**12 :.2g} & {D :.2g} $\pm$ {D_var :.2g} & {r*10**6 :.2g} $\pm$ {r_var*10**6 :.2g} \\\\")
+        r_var = 2.08 * 10**-19 / (np.exp(sigma**2)-1)*np.exp(-2*np.log(r) + sigma**2)**0.5
+        """
+        # fuck it
+        D = np.mean(Ds[:, 0])
+        D_var = np.std(Ds[:, 0])
+        r = 2.08 * 10**-19 / D
+        r_var = 2.08 * 10**-19 / D_var
+        print(f"{fileext} & {len(Ds)} & {D :.2g} $\pm$ {D_var :.2g} & {r*10**6 :.2g} $\pm$ {r_var*10**6 :.2g} \\\\")
 
 if __name__ == "__main__":
     main()
