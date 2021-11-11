1042510c4c598f99451b7dfd539d93bddc9c59a8
Viljar <viljar.femoen@hotmail.no>
2021-11-11 11:58:18 +0100

    Change the comp plot

--- TFY4335_bionano/lab/lab.py
+++ TFY4335_bionano/lab/lab.py
@@ -7,6 +7,9 @@ from matplotlib import pyplot as plt
 import os
 from PIL import Image
 
+#import matplotlib
+#matplotlib.rcParams['text.usetex'] = True
+
 METERS_PER_PIXEL = (500*10**-6) / 210.5
 a = 3/4*(75*500)*10**-12
 q = 8/3*10**-9
@@ -75,11 +78,13 @@ def eq_11_ij(D: float, i: int, j: int) -> float:
     error = -1 / 2* (-erf(1 / 2 *y *sqrt(q / (D *a *x))) + 1) + A
     return factor*exponent*error
 
-def eq11(D: float) -> float:
+def eq11(D: float, x: int = None) -> float:
     """Sum over i and j"""
+    if x is None:
+        x = range(DATA_SHAPE[1])
     res = 0
     for j in range(DATA_SHAPE[0]):
-        for i in range(DATA_SHAPE[1]):
+        for i in x:#
             if A_ij(i,j) == ma.masked:
                 continue
             res += eq_11_ij(D, i, j)
@@ -101,29 +106,29 @@ def optimize_this(D: float) -> float:
             res += (est_ij(D, i, j) - eq_11_ij(D, i, j))**2
     return res
 
-def d_eq11(D: float) -> float:
+def d_eq11(D: float, i) -> float:
     """Numerical derivation"""
     dD = 10**-17
-    return (eq11(D+dD) - eq11(D))/dD
+    return (eq11(D+dD, i) - eq11(D, i))/dD
 
-def D_next(D_n):
+def D_next(D_n, i):
     """Newton's method"""
-    return D_n - eq11(D_n) / d_eq11(D_n)
+    return D_n - eq11(D_n, i) / d_eq11(D_n, i)
 
-def get_D(D_0: float = None, rel_error: float = None) -> float:
+def get_D(D_0: float = None, rel_error: float = None, i = None) -> float:
     """Calculate D with Newton's method"""
     if D_0 is None:
         D_0 = 6.1*10**-8
     if rel_error is None:
         rel_error = 10**-4
-    Dn = D_next(D_0)
+    Dn = D_next(D_0, i)
     D = D_0
     n = 0
     while abs(1 - Dn / D) > rel_error:
-        e = abs(1-D/Dn)
-        print(f"{n} & {D :.4g} & {e :.4g} \\\\")
+        #e = abs(1-D/Dn)
+        #print(f"{n} & {D :.4g} & {e :.4g} \\\\")
         n += 1
-        D, Dn = D_next(Dn), D
+        D, Dn = D_next(Dn, i), D
     e = abs(1-D/Dn)
     print(f"{n} & {D :.4g} & {e :.4g} \\\\")
     return D
@@ -142,16 +147,30 @@ def scipy_minimize(D_0) -> float:
     return optimize.minimize(optimize_this, D_0)
 
 def main():
+    """
+    D = []
+    for i in range(100, DATA_SHAPE[1]):
+        if i < 100:
+            s = 10**-6
+        else:
+            s = 10**-9
+        D.append(get_D(s, i = [i]))
+        print(D[-1])
+    D = np.array(D)
+
+    print(np.count_nonzero(np.isnan(D)))
+    print(np.mean(D[~np.isnan(D)]))
+    print(np.std(D[~np.isnan(D)]))
     #"""
     #print(scipy_minimize(10**-10))
     D = get_D(10**-9)
     r = 2.08 * 10**-19 / D
     print(f"{D = :.2g}, {r = :.2g}")
-    #"""
+    """
     x = np.logspace(-11, -9, 1000, base = 10)
     y = eq11(x)
-    plt.plot(x, y)
-    plt.xlabel("D ($\\frac{m^2}{s}$)")
+    plt.plot(10**12*x, y)
+    plt.xlabel("D ($\\frac{µm^2}{s}$)")
     plt.ylabel("f(D)")
     plt.xscale("log")
     plt.title("Plot of equation 11")
@@ -170,32 +189,32 @@ def main():
     ax1.set_title(f"A: x = {round(x_i(20)*10**6)}µm (20 px)")
     ax1.set_xlabel("y (m)")
     ax1.set_ylabel("Intensity")
-    I, e = find_error([A_ij(x1, j) for j in y], lambda y: est_ij(D, x1, y))
-    ax1.text(x = y_j(40), y = 0.5, s = f"E: {I :.3g}$\pm0$")
+    #I, e = find_error([A_ij(x1, j) for j in y], lambda y: est_ij(D, x1, y))
+    ax1.text(x = y_j(40), y = 0.5, s = f"D: 2.8·10³ \nµm² / s")
 
     ax2.scatter(y_plot, [A_ij(x2, j) for j in y], s = 4)
     ax2.plot(y_plot_c, est_ij(D, x2, y_c), "m")
     ax2.set_title(f"B: x = {round(x_i(200)*10**6)}µm (200 px)")
     ax2.set_xlabel("y (m)")
     ax2.set_ylabel("Intensity")
-    I, e = find_error([A_ij(x2, j) for j in y], lambda y: est_ij(D, x2, y))
-    ax2.text(x = y_j(40), y = 0.5, s = f"E: {I :.3g}$\pm0$")
+    #I, e = find_error([A_ij(x2, j) for j in y], lambda y: est_ij(D, x2, y))
+    ax2.text(x = y_j(40), y = 0.5, s = f"D: 0.55·10³ \nµm² / s")
     
     ax3.scatter(y_plot, [A_ij(x3, j) for j in y], s = 4)
     ax3.plot(y_plot_c, est_ij(D, x3, y_c), "m")
     ax3.set_title(f"C: x = {round(x_i(500)*10**6)}µm (500 px)")
     ax3.set_xlabel("y (m)")
     ax3.set_ylabel("Intensity")
-    I, e = find_error([A_ij(x3, j) for j in y], lambda y: est_ij(D, x3, y))
-    ax3.text(x = y_j(40), y = 0.5, s = f"E: {I :.3g}$\pm0$")
+    #I, e = find_error([A_ij(x3, j) for j in y], lambda y: est_ij(D, x3, y))
+    ax3.text(x = y_j(40), y = 0.5, s = f"D: 1.1·10³ \nµm² / s")
 
     ax4.scatter(y_plot, [A_ij(x4, j) for j in y], s = 4)
     ax4.plot(y_plot_c, est_ij(D, x4, y_c), "m")
     ax4.set_title(f"D: x = {round(x_i(900)*10**6)}µm (900 px)")
     ax4.set_xlabel("y (m)")
     ax4.set_ylabel("Intensity")
-    I, e = find_error([A_ij(x4, j) for j in y], lambda y: est_ij(D, x4, y))
-    ax4.text(x = y_j(40), y = 0.5, s = f"E: {I :.3g}$\pm0$")
+    #I, e = find_error([A_ij(x4, j) for j in y], lambda y: est_ij(D, x4, y))
+    ax4.text(x = y_j(40), y = 0.5, s = f"D: 0.36·10³ \nµm² / s")
 
     plt.legend(["Model", "Data"])
     fig.suptitle("Comparing model values to data at different points along the channel")
