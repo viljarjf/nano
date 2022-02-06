4684cca4d0a0a2954514d4dd4cf5abf7e7a61cc2
Viljar Femoen <viljar.femoen@hotmail.no>
2022-02-06 01:11:31 +0100

    Very close to something I think

--- TFY4235_numfys/1/__main__.py
+++ TFY4235_numfys/1/__main__.py
@@ -5,20 +5,39 @@ import numpy as np
 
 def main():
     start = np.array([[0, 1, 1, 0], [0, 0, 1, 1]], dtype = np.float32)
-    single_line = np.array([[0, 12], [0, 0]], dtype = np.float32)
 
     new = start
-    l = 6
+    l = 2
     for i in range(l):
         new = generate.generate_next_level(new)
         print(f"Iteration {i+1} done")
 
     new = np.hstack((new, [[new[0, 0]], [new[1,0]]]))
-    plt.plot(new[0, :], new[1, :], c = "b")
-
-    print(properties.is_inside(np.array([0.7, 0.33]), start))
-    plt.show()
+    lattice = generate.generate_lattice(start, l+1)
+    l_pts = lattice.shape[1]
+    for i in range(lattice.shape[1]):
+        if properties.is_inside(lattice[:, i], start) == -1:
+            lattice[:, i] = [0, 0]
+            l_pts -= 1
     
+    print(f"corners: {new.shape[1]}\ncount: {l_pts}\nfraction: {l_pts / new.shape[1] * 100 :.1f}%")
+
+    plt.plot(new[0, :], new[1, :], c = "b")
+    plt.scatter(lattice[0, :], lattice[1, :], c = "g")
+    plt.show()
+
+    x = np.linspace(0.1, 0.2, 1024)
+    y = np.linspace(0.4, 0.5, 1024)
+    xx, yy = np.meshgrid(x, y)
+
+    zz = xx.copy()
+    p = np.array([xx, yy])
+    for i in range(xx.shape[0]):
+        for j in range(xx.shape[1]):
+            zz[i, j] = properties.is_inside(p[:, i, j], start)
+    print(np.count_nonzero(zz == 0))
+    plt.pcolormesh(x, y, zz, shading="auto")
+    plt.show()
     return
     dist = []
     for i in range(lattice.shape[1]):
--- TFY4235_numfys/1/properties.py
+++ TFY4235_numfys/1/properties.py
@@ -53,8 +53,8 @@ def distance_estimate(p: np.ndarray, start: np.ndarray, l: int, new) -> float:
     
     return d
 
-
-def is_inside(p: np.ndarray, start: np.ndarray) -> int:
+@numba.njit
+def is_inside(point: np.ndarray, start: np.ndarray) -> int:
     """returns whether a point is inside or outside the curve
 
     Args:
@@ -64,85 +64,91 @@ def is_inside(p: np.ndarray, start: np.ndarray) -> int:
     Returns:
         int: 1 if inside, -1 if outside, 0 if on border
     """
-    plt.scatter(p[0], p[1], c="r")
     # transform to an easy-to-work-with basis
     p0 = start[:, 0]
-    p = p - p0
 
+    # epsilon: small number to negate floating point precision
+    e = 0.0001
+
+    p = point - p0
     a = np.linalg.norm(p0 - start[:, 1])
     b = np.linalg.norm(start[:, -1] - p0)
     ct = (start[:, -1] - p0)[1] / b
     st = np.math.sin(np.math.acos(ct))
-    rotate_and_scale = np.array([[a*ct, a*st], [-b*st, b*ct]])
-    p = np.dot(rotate_and_scale, p)
-
+    p = np.array([
+        a*ct *p[0] + a*st*p[1], 
+        -b*st*p[0] + b*ct*p[1]
+        ])
+    
     # now we need to check if the point p is inside or outside the standard 1x1 fractal
 
     # first, check the largest bounding box
     bounding_box = np.array([[0, -1, 0, 1, 3, 4, 3, 2], [0, 1, 3, 4, 3, 2, 0, -1]])
     small_bounding_box = np.array([[0, 1, 3, 2], [0, 1, 0, -1]])
     smaller_bounding_boxes = np.array([
-                [
-                    [0, 1, 3, 2], 
-                    [0, 1, 0, -1]
-                ],[
-                    [3, 2, 3, 4], 
-                    [0, 1, 3, 2]
-                ], [
-                    [3, 4, 6, 5], 
-                    [3, 4, 3, 2]
-                ],[
-                    [6, 7, 6, 5], 
-                    [3, 2, 0, 1]
-                ], [
-                    [6, 7, 6, 5], 
-                    [0, -1, -3, -2]
-                ]
-            ])
+        [
+            [0, 1, 3, 2], 
+            [0, 1, 0, -1]
+        ],[
+            [3, 2, 3, 4], 
+            [0, 1, 3, 2]
+        ], [
+            [3, 4, 6, 5], 
+            [3, 4, 3, 2]
+        ],[
+            [6, 7, 6, 5], 
+            [3, 2, 0, 1]
+        ], [
+            [6, 7, 6, 5], 
+            [0, -1, -3, -2]
+        ]
+    ])
     final_polygon = np.array([
-            [1, 3, 2, 3],
-            [1, 0, 1, 3]
-        ])
-    is_in_bounding_box = utils.is_inside_polygon(3*p, bounding_box)
-    if is_in_bounding_box == 1:
+        [1, 3, 2, 3],
+        [1, 0, 1, 3]
+    ])
+    if utils.is_inside_convex_quadrilateral(3*p, bounding_box) >= 0:
         # inside the box, need more processing
 
         # if it is on a diagonal, it is guaranteed to be inside 
             # this also ensures that we don't need to consider edge cases of points being on a diagonal
-        if abs(p[0]) == abs(p[1]):
+        if np.abs(p[0]) == np.abs(p[1]):
             return 1
 
         # we know everything is rotationally symmetric around (0.5, 0.5), so we can just tansform the point down
         
         # above y=x, we rotate 180
-        if p[1] > p[0]:
+        if p[1] >= p[0]:
             p = 1-p
-
+        
         # above y=1-x, rotate 90 deg cockwise
-        if p[1] > 1-p[0]:
-            p[0], p[1] = p[1] - 0.5, 1 - p[0]
+        if p[1] >= 1-p[0]:
+            p[0], p[1] = p[1], 1 - p[0]
         
         # if now outside the small bounding box, it is inside the big bounding box
-        if utils.is_inside_polygon(3*p, small_bounding_box) == -1:
+        if utils.is_inside_convex_quadrilateral(3*p, small_bounding_box) == -1:
             return 1
         
         # if we reach this point, we need to loop down on the single fractal
         # to see if any of the 8 smaller bounding boxes contain the point
+        flip_side = 1
         while True:
             # to reduce our work, we flip everything to the right of the middle
             # remember to also flip whether we are outside or inside
-            flip_side = 1
             if p[0] > 0.5:
-                p = 1-p
-                flip_side = -1
+                p = np.array([1, 0])-p
+                flip_side *= -1
+
+            # check if we are on the curve
+            if abs(p[1]) < e and (abs(p[0]) < e or abs(p[0] - 1) < e):
+                return 0
             
             # check the 5 smaller bounding boxes
             # also, multiply by 12 to work with integers for the bounding box
             p *= 12
             break_loop = False
             for i in range(smaller_bounding_boxes.shape[0]):
-                is_in_bounding_box = utils.is_inside_polygon(p, smaller_bounding_boxes[i, :, :])
-                if is_in_bounding_box > -1:
+                if utils.is_inside_convex_quadrilateral(p, smaller_bounding_boxes[i, :, :]) >= 0:
                     # we are inside (or on the edge of) a smaller box! Transform a little and try again
                     p0 = smaller_bounding_boxes[i, :, 0]
                     p1 = smaller_bounding_boxes[i, :, 2] - p0
@@ -152,12 +158,9 @@ def is_inside(p: np.ndarray, start: np.ndarray) -> int:
 
                     elif p1[1] < 0:
                         p[0], p[1] = -p[1], p[0]
-                    
-                    # check if we are on the curve
-                    if np.all(p == p0) or np.all(p == p1):
-                        return 0
 
-                    p /= 12
+                    p /= 3 # 12/3 = 4 in total
+                    
                     break_loop = True
                     break
             if break_loop:
@@ -171,4 +174,4 @@ def is_inside(p: np.ndarray, start: np.ndarray) -> int:
             # we are inside even if we are on the edge of this polygon
             return flip_side
     else:
-        return is_in_bounding_box
+        return -1
