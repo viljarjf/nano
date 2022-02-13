a7a24035125dd958072268e8ae621f48200027f6
Viljar Femoen <viljar.femoen@hotmail.no>
2022-02-13 20:37:01 +0100

    Tried cupy, there's a bug in the sparse eigen-solver

--- TFY4235_numfys/1/__main__.py
+++ TFY4235_numfys/1/__main__.py
@@ -1,3 +1,4 @@
+import os
 from . import generate, properties, utils
 
 from matplotlib import pyplot as plt
@@ -5,18 +6,22 @@ import numpy as np
 
 from scipy.sparse import linalg
 
+#os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+#from cupyx.scipy.sparse import linalg
+#import cupy
+
 def main():    
     start = np.array([[0, 1, 1, 0], [0, 0, 1, 1]], dtype = np.float32)
+    l = 4
 
     new = start
-    l = 4
     for i in range(l):
         new = generate.generate_next_level(new)
         print(f"Iteration {i+1} done")
-    
-    print(properties.is_inside(np.array([0.59375, 0.03125]), start))
+        
+    # not strictly necessary, but compiling the function doesn't hurt
+    properties.is_inside(np.array([0.59375, 0.03125]), start)
 
-    new = np.hstack((new, [[new[0, 0]], [new[1,0]]]))
     lattice = generate.generate_lattice(start, l)
     
     print("Starting matrix setup")
@@ -29,14 +34,37 @@ def main():
         for j in range(n):
             if properties.is_inside(lattice[:, i, j], start) != 1:
                 properties.set_zero(i, j, n, A)
+    
+    #A = cupy.sparse.coo_matrix(A)
+    A = A.asformat("coo")
 
     print("Finished marix setup")
 
-    roots, v = linalg.eigsh(A.asformat("coo"), k = 10)
-    roots *= h**2
-    print(roots)
+    print("Starting to solve the eigenproblem")
     
+    #gpu_roots, gpu_v = linalg.eigsh(A, k = 100)
+    #roots = gpu_roots.get()
+    #v = gpu_v.get()
+    roots, v = linalg.eigsh(A.asformat("coo"), k = 100)
 
+    print("Finished solving the eigenproblem")
+    roots *= -h**2
+    roots **= 0.5
+    
+    figdir = os.path.join(os.path.dirname(__file__), "figures")
+    if not os.path.exists(figdir):
+        os.makedirs(figdir)
+
+    print("Starting to make figures")
+    x = np.unique(lattice[0, :, :])
+    y = np.unique(lattice[1, :, :])
+    for i in range(v.shape[1]):
+        plt.figure(figsize = (4, 4), dpi = 250)
+        plt.pcolormesh(x, y, np.transpose(v[:, i].reshape(n, n)**2))
+        plt.plot(new[0, :], new[1, :], )
+        plt.savefig(os.path.join(figdir, f"Eigenvector #{i}.png"), )
+        plt.close()
+    print("Done")
 
 if __name__ == "__main__":
     main()
