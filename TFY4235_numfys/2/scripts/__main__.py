945a84af7d1fb0d28a174eb683e9e654aee60566
Viljar Femoen <viljar.femoen@hotmail.no>
2022-04-17 13:44:57 +0200

    Plot new data

--- TFY4235_numfys/2/scripts/__main__.py
+++ TFY4235_numfys/2/scripts/__main__.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import os
 
 from . import plot
 
@@ -9,11 +10,19 @@ def main():
     #plt.plot(np.linspace(0, 1, len(d)), d)
     #for i in range(10):
     #    plt.hist(d[i], bins =300)# np.linspace(0.75, 1, 300))
-
+    U = np.load(os.path.join(os.path.dirname(__file__), "..", "data", "potential.npy"))
     p = [1, 3, 6]
+    plt.figure()
+    plt.subplot(311)
     for i in p:
         plt.plot(t[i, :], d[i, :])
     plt.legend(p)
+    
+    plt.subplot(312)
+    plt.plot(t[0,:], U[::10,0])
+    
+    plt.subplot(313)
+    plt.plot(t[0,:], U[::10,1])
     plt.show()
 
 if __name__ == "__main__":
