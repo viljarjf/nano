988d852971aac07768a65db51d6c10f4393a3135
Viljar Femoen <viljar.femoen@hotmail.no>
2022-04-20 10:33:30 +0200

    copy ass 2

---
+++ TFY4235_numfys/3/include/euler.h
@@ -0,0 +1,17 @@
+#ifndef BBM_EULER_H
+#define BBM_EULER_H
+
+#include "constants.h"
+#include "reduced_physics.h"
+#include "rng.h"
+
+/**
+ * @brief Implement the Euler scheme in eq. 2.9
+ * 
+ * @param x Coordinate, will be updated
+ * @param t Time, will be updated
+ * @param r Particle radius
+ */
+void euler_scheme(double *x_hat, double *t, reduced_constants_t *red_consts);
+
+#endif
