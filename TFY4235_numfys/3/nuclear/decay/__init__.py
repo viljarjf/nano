3ddacb2d72d9bd939f61de51caacdc34c4c207bd
Viljar Femoen <viljar.femoen@hotmail.no>
2022-04-26 09:16:50 +0200

    Implement analytical decay. Not working properly

--- TFY4235_numfys/3/nuclear/decay/__init__.py
+++ TFY4235_numfys/3/nuclear/decay/__init__.py
@@ -3,18 +3,48 @@
 from nuclear.decay.probability import decay
 
 import numpy as np
+from typing import Generator
 
-def run(Ns: list[int], lambdas: list[float], dt: float) -> None:
-    """Run the decay algorithm for a single timestep. Mutates the Ns
+def run(Ns0: list[int], lambdas: list[float], dt: float, n: int) -> Generator[list[int], None, None]:
+    """Run the decay algorithm for `n` timesteps. 
 
     Args:
-        Ns (list[int]): List of particle amounts. Needs an extra element at the end to keep the leftovers
+        Ns0 (list[int]): List of initial particle amounts. Needs an extra element at the end to keep the leftovers
         lambdas (list[float]): probabilities of decaying
         dt (float): discrete timestep
+        n(int): steps
     """
-    for i, l in enumerate(lambdas):
-        for _ in range(Ns[i]):
-            if decay(dt, l):
-                Ns[i] -= 1
-                Ns[i+1] += 1 # this is fine as the Ns contain a dead-end failsafe as a last element
+    Ns = Ns0
+    for _ in range(n):
+        for i, l in enumerate(lambdas):
+            for _ in range(Ns[i]):
+                if decay(dt, l):
+                    Ns[i] -= 1
+                    Ns[i+1] += 1 # this is fine as the Ns contain a dead-end failsafe as a last element
+        yield Ns
+
+
+def run_analytical(N0: list[int], lambdas: list[float], t: float) -> list[float]:
+    """Calcualte the state at time `t`. Mutates the Ns
+
+    Args:
+        N0 (list[int]): Initial particle amount of particle 0
+        lambdas (list[float]): probabilities of decaying
+        t (float): Time at which to calculate the state
+    
+    Returns:
+        list[float]: amounts of each atom. Can be non-integral
+    """
+    Ns = [N0*np.exp(-lambdas[0]*t)]
+    
+    prod = lambda l: l[0]*prod(l[1:]) if l else 1
+    diffsum = lambda l, lk: prod([li - lk for li in l])
+    hk = lambda k: prod(lambdas[:k]) / diffsum(lambdas[:k], lambdas[k])
+
+    for i in range(1, len(lambdas)):
+        Ns.append(N0 * sum([hk(i)*np.exp(-lambdas[i]*t)]))
+
+    Ns.append(N0 - sum(Ns))
+
     return Ns
+        
--- TFY4235_numfys/3/tests/qualitative/nuclear_decay.py
+++ TFY4235_numfys/3/tests/qualitative/nuclear_decay.py
@@ -5,34 +5,44 @@ from nuclear.decay.utils import steps, half_life
 from matplotlib import pyplot as plt
 
 
-def plot_nuclear_decay(Ns0, lambdas, dt):
+def plot_nuclear_decay(N0, lambdas, dt):
+    Ns0 = [N0] + [0]*len(lambdas)
     Nsi = Ns0
     n = steps(lambdas[0], 0.001, dt)
 
     t = [0]
-    Ns = [[Nsi[0]], [Nsi[1]], [Nsi[2]]]
-    for _ in range(n):
-        decay.run(Nsi, lambdas, dt)
+    Ns = [[Ns0[0]], [Ns0[1]], [Ns0[2]]]
+    for Nsi in decay.run(Nsi, lambdas, dt, n):
         t.append(t[-1] + dt)
         Ns[0].append(Nsi[0])
         Ns[1].append(Nsi[1])
         Ns[2].append(Nsi[2])
     
+    Nsa = [[], [],[]]
+    for ti in t:
+        Nsai = decay.run_analytical(N0, lambdas, ti)
+        print(Nsai)
+        Nsa[0].append(Nsai[0])
+        Nsa[1].append(Nsai[1])
+        Nsa[2].append(Nsai[2])
+
     plt.figure()
     plt.plot(t, Ns[0])
     plt.plot(t, Ns[1])
     plt.plot(t, Ns[2])
-    plt.legend(["0", "1", "2"])
+    plt.plot(t, Nsa[0])
+    plt.plot(t, Nsa[1])
+    plt.plot(t, Nsa[2])
+    plt.legend(["0", "1", "2", "Analytical 0", "Analytical 1", "Analytical 2"])
     plt.title(f"Nuclear decay over time\n$\lambda_0 = {lambdas[0]}$, $\lambda_1 = {lambdas[1]}$")
     plt.xlabel("Time")
     plt.ylabel("Amount")
     plt.show()
 
 def main():
-    
-    Ns0 = [1000, 0, 0]
+    N0 = 1000
     lambdas = [10, 2]
     dt = 0.001
     for l in lambdas:
         print(f"lambda: {l}, Half-life: {half_life(l)}")
-    plot_nuclear_decay(Ns0, lambdas, dt)
+    plot_nuclear_decay(N0, lambdas, dt)
