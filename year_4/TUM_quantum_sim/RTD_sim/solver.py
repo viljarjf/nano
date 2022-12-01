c40414470d2295cde175a8423135e6331a12cf45
Viljar Femoen <viljar.femoen@hotmail.no>
2022-12-01 16:03:28 +0100

    there was a single typo in transfer_matrix._M0

--- year_4/TUM_quantum_sim/RTD_sim/solver.py
+++ year_4/TUM_quantum_sim/RTD_sim/solver.py
@@ -2,19 +2,21 @@
 import numpy as np
 
 from RTD_sim.system import System
-from RTD_sim.transfer_matrix import Td, TransferMatrix
+from RTD_sim.transfer_matrix import M, TransferMatrix
 
 def _T_mat_tot(E: float, sys: System, N: int) -> TransferMatrix:
     z = np.linspace(0, sys.L, N)
-    k1 = sys.k(E, 0)
-    T_final = np.eye(2)
-    for i in z:
-        k2 = sys.k(E, i)
-        T_final = T_final @ Td(k1, k2, i)
+    dz = sys.L / N
+    T_final = np.eye(2, dtype=np.complex128)
+    for i in range(N-1):
+        k = sys.k(E, z[i])
+        beta1 = sys.beta(E, z[i])
+        beta2 = sys.beta(E, z[i+1])
+        T_final = M(k, beta1, beta2, dz) @ T_final
     return T_final
 
 def T(E: float, sys: System, N: int):
     T_mat = _T_mat_tot(E, sys, N)
     beta0 = sys.beta(E, 0)
     betaN = sys.beta(E, sys.L)
-    return betaN / beta0 * (np.linalg.det(T_mat) / T_mat[1,1])**2
+    return betaN / beta0 * np.abs(np.linalg.det(T_mat) / T_mat[1,1])**2
