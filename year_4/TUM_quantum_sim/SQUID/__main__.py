f4bd55969505fc6beb6706fc52dc38e938e81c26
Viljar Femoen <viljar.femoen@hotmail.no>
2023-01-10 10:38:18 +0100

    Initial commit

---
+++ year_4/TUM_quantum_sim/SQUID/__main__.py
@@ -0,0 +1,11 @@
+from SQUID import SQUID_LOGGER
+
+def main():
+    SQUID_LOGGER.info("Starting simulation")
+
+    # simulation goes here
+
+    SQUID_LOGGER.info("Simulation finished, exiting...")
+
+if __name__ == "__main__":
+    main()
