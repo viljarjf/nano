13b55b19d401bd521151d061bf748a8d5a2bcd5f
Viljar Femoen <viljar.femoen@hotmail.no>
2023-01-12 16:09:42 +0100

    Implement schrodinger-poisson

--- year_4/TUM_quantum_sim/__main__.py
+++ year_4/TUM_quantum_sim/__main__.py
@@ -2,10 +2,12 @@ from TUM_quantum_sim.QW_sim import main as QW
 from TUM_quantum_sim.RTD_sim import main as RTD
 from TUM_quantum_sim.SQUID import main as SQUID
 from TUM_quantum_sim.Dual_QW import main as Dual_QW
+from TUM_quantum_sim.QCL import main as QCL
 
 if __name__ == "__main__":
     # RTD.main()
     # QW.main()
     # SQUID.main()
-    Dual_QW.main()
+    # Dual_QW.main()
+    QCL.main()
     pass
