2f991e607cd80dec019da4bcc4830e367e455e12
Viljar Femoen <viljar.femoen@hotmail.no>
2023-01-20 17:10:57 +0100

    Properly ensure copying works

--- year_4/TUM_quantum_sim/exam/sparse_override.py
+++ year_4/TUM_quantum_sim/exam/sparse_override.py
@@ -10,11 +10,11 @@ import numpy as np
 class Hamiltonian(sp.dia_matrix):
 
     def add_static_potential(self, V0: np.ndarray):
-        self.__default_data = self.data
-        self.__default_data[1, :] += V0
+        self.data[1, :] += V0
+        self.__default_data = self.data.copy()
     
-    def add(self, potential) -> Hamiltonian:
-            self.data = self.__default_data
+    def add(self, potential: np.ndarray) -> Hamiltonian:
+            self.data = self.__default_data.copy()
             # index 1 is the central diagonal
             self.data[1, :] += potential
             return self
