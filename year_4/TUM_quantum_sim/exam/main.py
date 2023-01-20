830b4bc7f263c560a6feb457a4740621b50fb1d1
Viljar Femoen <viljar.femoen@hotmail.no>
2023-01-20 14:19:22 +0100

    Use new class, and correct dt estimate

--- year_4/TUM_quantum_sim/exam/main.py
+++ year_4/TUM_quantum_sim/exam/main.py
@@ -3,6 +3,7 @@ from TUM_quantum_sim.exam import EXAM_LOGGER as logging
 from TUM_quantum_sim import constants as c
 from TUM_quantum_sim.exam import potential
 from TUM_quantum_sim.exam import plot
+from TUM_quantum_sim.exam.sparse_override import Hamiltonian
 
 import numpy as np
 from scipy import sparse as sp
@@ -14,13 +15,13 @@ def main():
     # simulation setup
     a = 1e-9
     L = 2*a
-    N = 51              
+    N = 50
     m = c.me
     Vb = c.e0
     n_states = 2
     E = 1e9
     omega = np.pi*200e12
-    t_end = 1000e-15    # end of simulation
+    t_end = 100e-15     # end of simulation
     t_store = 2e-15     # time between each data storage
 
     E = omega = 0       # override 
@@ -37,14 +38,13 @@ def main():
 
     # hamiltonian
     h0 = -c.hbar**2 / (2 * m * dz**2)
-    H0 = h0 * sp.diags(
+    H0 = Hamiltonian(h0 * sp.diags(
         [1, -2, 1], [-1, 0, 1], 
         shape=(N, N), 
         dtype=np.complex128, 
-        format="csc"
-        )
-    H0 += sp.diags(V0)
-
+        format="dia"
+        ))
+    H0 = H0 + V0
     # plot.H(H0)
 
     # find the smallest (algebraic, not in absolute value) eigenvalues
@@ -55,13 +55,18 @@ def main():
 
     logging.info("Performing setup for temporal simulation")
     # estimate a decent dt from lectures
-    dt = 0.25 * (np.max(V0) + 4 * abs(h0))
+    V_max = V0 + potential.temporal(z, np.pi, E, 1)
+    E_max = max(
+        abs(min(V_max)),
+        abs(max(V_max) + 4 * abs(h0)),
+        )
+    dt = 0.5 * c.hbar / E_max
     logging.info(f"{dt = :.2e} s")
 
     # calculate initial psi
     psi_half = (_psi[:, 0] + _psi[:, 1]) * 2**-0.5
     V_half = potential.temporal(z, dt/2, E, omega)
-    H_half = dt/(2j * c.hbar) * (H0 + sp.diags(V_half)) @ psi_half
+    H_half = dt/(2j * c.hbar) * (H0 + V_half) @ psi_half
     psi_0 = psi_half - H_half
     psi_1 = psi_half + H_half
     psi = [np.abs(psi_0)**2]
@@ -79,7 +84,7 @@ def main():
         # H^n = H0 + V^n
 
         Vt = potential.temporal(z, tn, E, omega)
-        H = 2*dt/(1j * c.hbar) * (H0 + sp.diags(Vt))
+        H = 2*dt/(1j * c.hbar) * (H0 + Vt)
 
         psi_2 = H @ psi_1 + psi_0
 
@@ -103,7 +108,7 @@ def main():
 
     # plot.psi2_3D(z, t, psi)
 
-    # plot.psi2_animation(z, V, psi)
+    plot.psi2_animation(z, V, psi)
     
     # plot.psi2_z(t, np.nonzero(z == 0.0)[0], psi)
 
