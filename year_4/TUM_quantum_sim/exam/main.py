e150a69faf40f0b82fe08b717676ee0c32d5bf17
Viljar Femoen <viljar.femoen@hotmail.no>
2023-01-20 16:03:31 +0100

    Fix logic so potential is not added multiple times

--- year_4/TUM_quantum_sim/exam/main.py
+++ year_4/TUM_quantum_sim/exam/main.py
@@ -38,13 +38,13 @@ def main():
 
     # hamiltonian
     h0 = -c.hbar**2 / (2 * m * dz**2)
-    H0 = Hamiltonian(h0 * sp.diags(
-        [1, -2, 1], [-1, 0, 1], 
+    H0 = Hamiltonian(sp.diags(
+        [h0, -2*h0, h0], [-1, 0, 1], 
         shape=(N, N), 
         dtype=np.complex128, 
         format="dia"
         ))
-    H0 = H0 + V0
+    H0.add_static_potential(V0)
     # plot.H(H0)
 
     # find the smallest (algebraic, not in absolute value) eigenvalues
@@ -66,7 +66,7 @@ def main():
     # calculate initial psi
     psi_half = (_psi[:, 0] + _psi[:, 1]) * 2**-0.5
     V_half = potential.temporal(z, dt/2, E, omega)
-    H_half = (H0 + V_half) @ (dt/(2j * c.hbar) * psi_half)
+    H_half = H0.add(V_half) @ (dt/(2j * c.hbar) * psi_half)
     psi_0 = psi_half - H_half
     psi_1 = psi_half + H_half
     psi = [np.abs(psi_0)**2]
@@ -76,6 +76,7 @@ def main():
     V = [V0]    # array for storing potentials
 
     logging.info(f"Starting temporal simulation for {t_end / 1e-15 :.1f} fs")
+    coef = 2*dt/(1j * c.hbar)
     while tn < t_end:
         
         # Leapfrog
@@ -85,7 +86,7 @@ def main():
 
         Vt = potential.temporal(z, tn, E, omega)
 
-        psi_2 = (H0 + Vt) @ (2*dt/(1j * c.hbar) * psi_1) + psi_0
+        psi_2 = H0.add(Vt) @ (coef * psi_1) + psi_0
 
         psi_0, psi_1 = psi_1, psi_2
 
--- year_4/TUM_quantum_sim/exam/sparse_override.py
+++ year_4/TUM_quantum_sim/exam/sparse_override.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 """
 
 Custom class to hopefully speed up hamiltonian addition
@@ -8,9 +9,12 @@ import numpy as np
 
 class Hamiltonian(sp.dia_matrix):
 
-    def __add__(self, other):
-        if isinstance(other, np.ndarray):
-            self.setdiag(self.diagonal(0) + other)
+    def add_static_potential(self, V0: np.ndarray):
+        self.__default_data = self.data
+        self.__default_data[1, :] += V0
+    
+    def add(self, potential) -> Hamiltonian:
+            self.data = self.__default_data
+            # index 1 is the central diagonal
+            self.data[1, :] += potential
             return self
-        else:
-            return super().__add__(other)
