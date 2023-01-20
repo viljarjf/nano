872659d90db6872d15ab8d575ad5376c1d4682d7
Viljar Femoen <viljar.femoen@hotmail.no>
2023-01-20 13:13:47 +0100

    Change plots to input abs(psi)^2

--- year_4/TUM_quantum_sim/exam/plot.py
+++ year_4/TUM_quantum_sim/exam/plot.py
@@ -39,20 +39,20 @@ def psi(z: np.ndarray, E: np.ndarray, psi: np.ndarray):
     plt.tight_layout()
     plt.show()
 
-def psi_3D(z: np.ndarray, t: np.ndarray, psi: np.ndarray):
+def psi2_3D(z: np.ndarray, t: np.ndarray, abs_psi_squared: np.ndarray):
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     X, Y = np.meshgrid(z, t)
-    surf = ax.plot_surface(X, Y, abs(psi)**2, cmap=cm.get_cmap("viridis"),linewidth=0, antialiased=False)
+    surf = ax.plot_surface(X, Y, abs_psi_squared, cmap=cm.get_cmap("viridis"),linewidth=0, antialiased=False)
     plt.xlabel("z [m]")
     plt.ylabel("t [s]")
     plt.title("$|\Psi|^2$")
     fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
 
-def psi_animation(z: np.ndarray, V: np.ndarray, psi: np.ndarray):
+def psi2_animation(z: np.ndarray, V: np.ndarray, abs_psi_squared: np.ndarray):
     fig, (ax1, ax2) = plt.subplots(2, 1)
 
-    ln_psi, = ax1.plot(z, abs(psi[0, :])**2)
+    ln_psi, = ax1.plot(z, abs_psi_squared[0, :])
     ln_V, = ax2.plot(z, V[0, :])
 
     def init():
@@ -64,11 +64,11 @@ def psi_animation(z: np.ndarray, V: np.ndarray, psi: np.ndarray):
         return ln_psi, ln_V,
 
     def frames():
-        for n in range(psi.shape[0]):
-            yield psi[n, :], V[n, :]
+        for n in range(abs_psi_squared.shape[0]):
+            yield abs_psi_squared[n, :], V[n, :]
 
     def update(data):
-        ln_psi.set_data(z, abs(data[0])**2)
+        ln_psi.set_data(z, data[0])
         ln_V.set_data(z, data[1] / c.e0)
         return ln_psi, ln_V,
 
