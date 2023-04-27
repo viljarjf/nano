fe6f3815b6094800d7bd7180b847489570d60621
Viljar Femoen <viljar.femoen@hotmail.no>
2023-04-27 16:53:03 +0200

    Initial setup

---
+++ year_4/TUM_quantum_nano_sim/01.py
@@ -0,0 +1,13 @@
+"""
+Computational Methods for Nanoelectronics: Quantum Models
+Exercise 1
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
